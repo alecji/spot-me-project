b0066ea7f2e76f7f5835fbc040c5b63feecc1fe1
Alec Inthalangsy <alecinthalangsy@Alecs-MacBook-Air.local>
2018-08-31 11:27:49 -0400

    fixed folder structure

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Router } from 'react-router-dom';
 import App from './App';
-import Home from './Home/Home';
-import Callback from './Callback/Callback';
-import Auth from './Auth/Auth';
-import history from './history';
+import Home from './pages/Home/Home';
+import Callback from './utils/Callback/Callback';
+import Auth from './utils/Auth/Auth';
+import history from './utils/history';
 
 const auth = new Auth();
 
