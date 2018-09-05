537e4485efcfedc9002be97d8cd1a031e73c9462
jyi428 <jyi428jh@gmail.com>
2018-09-05 13:28:19 -0400

    callback

--- client/src/utils/Auth/auth0-variables.js
+++ client/src/utils/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'j-car.auth0.com',
   clientId: '4NnFy3tPgXEc5RmYL26mFMH408UpRiT6',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://vast-journey-99071.herokuapp.com/callback'
 }
