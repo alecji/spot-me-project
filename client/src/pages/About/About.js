d25208b966f1f730e4e5f0c5308315aac529caec
Christian Chung <christianchung11@gmail.com>
2018-09-05 11:19:25 -0400

    about me

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,16 +1,12 @@
 import React from "react";
 
 const About = () => (
-  <div>
-    <h1>About Page</h1>
-    <p>
-      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
-    </p>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">About Us</h1>
+    <p class="lead">    Our goal is to make a community for people who are focused on improving themselves physically. Whether you are a veteran body builder or a complete novice, Spot Me is here to connect you to others who share your vision. If you need some advice go to the live chat room and ask somebody, if you want to record your fitness progress write an entry in your fitness journal! Keep in touch with others in your area and chat it up, who knows you may see them at your gym! And if you ever need a workout partner just tell them Spot Me!</p>
   </div>
+</div>
 );
 
 export default About;
